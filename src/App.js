e964ad6005a2ff57569fb34fe59af7c98c6a2e4d
Gabriel Wilkins <wilkigab001@gmail.com>
2022-11-11 17:53:40 -0700

    Final changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import Header from './components/Header';
-import Home from './components/Home';
-import './App.css';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import {Provider} from 'react-redux'
+import {store} from "./store";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Header />
+        <Home />
+      </div>
+    </Provider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,131 @@
-import React from 'react'
+import React, { useState } from "react";
+import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import {selectCars} from "../features/car/carSlice";
+import {useSelector} from "react-redux"
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [showSideBar, setShowSidebar] = useState(false);
+  const cars = useSelector(selectCars);
 
-export default Header
+  return (
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt="" />
+      </a>
+      <Menu>
+        {cars && cars.map((car, index) => {
+            return (<a href="#">{car}</a>)
+        })}
+      </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+        <CustomMenu onClick={() => setShowSidebar(true)} />
+      </RightMenu>
+      {showSideBar && 
+      <BurgerNav show={showSideBar}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setShowSidebar(false)} />
+        </CloseWrapper>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-in</a>
+        </li>
+        <li>
+          <a href="#">CyberTruck</a>
+        </li>
+        <li>
+          <a href="#">Roadster</a>
+        </li>
+        <li>
+          <a href="#">Roadster</a>
+        </li>
+      </BurgerNav>
+      }
+    </Container>
+  );
+};
+
+export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+`;
+
+const Menu = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: no-wrap;
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
+`;
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
